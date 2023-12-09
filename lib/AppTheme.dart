589dc52a6fb1ea27326020ef3bfb53fbd76ba4a5
Thiago <thiagomvas@gmail.com>
2023-12-09 11:31:42 -0300

    Add app theme class for easy customization

---
+++ lib/AppTheme.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color primaryColor = Color(0xFFBE3144);
+  static const Color secondaryColor = Color(0xFF872341);
+  static const Color backgroundColor = Color(0xFF22092C);
+  static const TextStyle topBarButtonTextStyle = TextStyle(
+    color: Colors.white,
+    fontSize: 16,
+  );
+  static const TextStyle titleTextStyle = TextStyle(
+    color: primaryColor,
+    fontSize: 32,
+    fontWeight: FontWeight.bold
+  );
+  
+  static const TextStyle h1TextStyle = TextStyle(
+    color: Colors.white,
+    fontSize: 40,
+  );
+  static const TextStyle h2TextStyle = TextStyle(
+    color: Colors.white,
+    fontSize: 32,
+  );
+
+  static const TextStyle nameTextStyle = TextStyle(
+    color: primaryColor,
+    fontSize: 40,
+    fontWeight: FontWeight.bold
+  );
+
+}
