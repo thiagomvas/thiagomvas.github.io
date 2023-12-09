c96623406702c23fbb3abbd744bf4f608efc37c4
Thiago <thiagomvas@gmail.com>
2023-12-09 13:41:04 -0300

    Icon Test

--- lib/Widgets/hero.dart
+++ lib/Widgets/hero.dart
@@ -78,8 +78,8 @@ class Title extends StatelessWidget {
         ),
         Row(
           children: [
-            CustomIconButton(orientation: orientation, text: 'GitHub', iconPath: "../assets/icons/github.png", onPressed: () {}),
-            CustomIconButton(orientation: orientation, text: 'LinkedIn', iconPath: "../assets/icons/linkedin.png", onPressed: () {}),
+            CustomIconButton(orientation: orientation, text: 'GitHub', iconPath: "assets/icons/github.png", onPressed: () {}),
+            CustomIconButton(orientation: orientation, text: 'LinkedIn', iconPath: "assets/icons/linkedin.png", onPressed: () {}),
           ],
         )
       ],
